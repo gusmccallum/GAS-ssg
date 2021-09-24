aba4c56e7d47895933c82ba30689587282204a0f
Joshua Li <49960519+joshuali7536@users.noreply.github.com>
2021-09-23 23:13:22 -0400

    Added .md functionality
    Markdown file reading functionality and can read "# " header syntax

--- gasMain.cpp
+++ gasMain.cpp
@@ -8,7 +8,7 @@
 
 static void processText(std::string fileName, int fileType);
 static void newFolder();
-
+std::string makeHeader1(std::string line);
 
 int main(int argc, char** argv) {
 
@@ -72,13 +72,25 @@ int main(int argc, char** argv) {
 			}
 
 		}
+		//md file input
+		else if (fileNameString.find(".md") != std::string::npos) {
+			std::ifstream inFile(fileNameString);
+			if (!inFile)
+			{
+				inFile.close();
+			}
+			else {
+				newFolder();
+				processText(fileNameString, 2);
+			}
+		}
 		else {
 			//folder input
 			newFolder();
 			for (const auto& dirItem : std::filesystem::recursive_directory_iterator(fileNameString)) {
 				std::string path = dirItem.path().string();
 				if (path.find(".txt") != std::string::npos) {
-					processText(path, 2);
+					processText(path, 3);
 				}
 			}
 		}
@@ -87,6 +99,7 @@ int main(int argc, char** argv) {
 
 static void processText(std::string fileName, int fileType) {
 	std::string outFileName = "";
+	std::string title = "";
 	//open input file
 	std::ifstream inFile(fileName);
 	if (!inFile) {
@@ -96,35 +109,55 @@ static void processText(std::string fileName, int fileType) {
 		outFileName = fileName;
 		outFileName = outFileName.substr(0, outFileName.find("."));
 	}
-	else if (fileType == 2) { //folder input
+	else if (fileType == 2) { //.md input
+		outFileName = fileName;
+		outFileName = outFileName.substr(0, outFileName.find("."));
+	}
+	else if (fileType == 3) { //folder input
 		outFileName = fileName;
 		outFileName = outFileName.erase(0, outFileName.find_last_of("\\") + 1);
 		outFileName = outFileName.substr(0, outFileName.find("."));
 	}
+	title = outFileName;
 	//open output file
 	outFileName = "./dist/" + outFileName + ".html";
 	std::ofstream outFile(outFileName);
 	if (outFile) {
 
 		outFile << " <!DOCTYPE html> <html> <br> <head>" << '\n';
-		//get Title
-		std::string line1 = "";
-		std::string line2 = "";
-		std::string line3 = "";
+		//.txt file title/header
+		if (fileType == 1) {
+			//get Title
+			std::string line1 = "";
+			std::string line2 = "";
+			std::string line3 = "";
 
-		std::getline(inFile, line1);
-		std::getline(inFile, line2);
-		std::getline(inFile, line3);
+			std::getline(inFile, line1);
+			std::getline(inFile, line2);
+			std::getline(inFile, line3);
 
-		//Set title if present
-		if (line1 != "" && line2 == "" && line3 == "") {
-			outFile << " <title> " << line1 << "</title>" << '\n';
+			//Set title if present
+			if (line1 != "" && line2 == "" && line3 == "") {
+				outFile << " <title> " << line1 << "</title>" << '\n';
+			}
+
+			outFile << "</head>" << '\n' << "<body>" << '\n' << "<h1> " << line1 << "</h1>" << '\n';
+		}
+		//.md file title
+		else if (fileType == 2) {
+			outFile << " <title> " << title << "</title>" << '\n';
+			outFile << "</head>" << '\n' << "<body>" << '\n';
 		}
 
 		std::string inLine = "";
 		std::string lastLine = "";
-		outFile << "</head>" << '\n' << "<body>" << '\n' << "<h1> " << line1 << "</h1>" << '\n';
 		while (std::getline(inFile, inLine)) {
+			if (fileType == 2) {
+				//Check for Header 1 syntax
+				if ((inLine.substr(0,2).find("# ") != std::string::npos)) {
+					inLine = makeHeader1(inLine);
+				}
+			}
 
 			if (inLine != "" && lastLine == "") {
 				outFile << "<p> " << '\n' << inLine << '\n';
@@ -146,6 +179,13 @@ static void processText(std::string fileName, int fileType) {
 		std::cout << "Error creating output file." << std::endl;
 	}
 
+}
+
+std::string makeHeader1(std::string line) {
+	std::string newLine = line;
+	newLine.erase(0, 2);
+	newLine = "<h1>" + newLine + "</h1>";
+	return newLine;
 }
 
 static void newFolder() {
