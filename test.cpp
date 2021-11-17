ed2a6793bee0b42cff218574868304145768dab4
Kien <kiennguyenchi11@gmail.com>
2021-11-17 13:58:04 -0500

    add test cases to check if it's not .txt/.md/.json/folder

--- test.cpp
+++ test.cpp
@@ -24,16 +24,36 @@ TEST_CASE("isText() Testing", "[istext]") {
 	REQUIRE(isText("The Adventure of the Speckled Band.txt") == true);
 }
 
+TEST_CASE("isText() Testing (not .txt file)", "[istext]") {
+	REQUIRE(isText("The Adventure of the Speckled Band.md") == false);
+	REQUIRE(isText("The Adventure of the Speckled Band") == false);
+}
+
 TEST_CASE("isMarkDown() Testing", "[ismarkdown]") {
 	REQUIRE(isMarkDown("readme.md") == true);
+}
+
+TEST_CASE("isMarkDown() Testing (not .md file)", "[ismarkdown]") {
+	REQUIRE(isMarkDown("The Adventure of the Speckled Band.txt") == false);
+	REQUIRE(isMarkDown("The Adventure of the Speckled Band") == false);
 }
 
 TEST_CASE("isFolder() Testing", "[isfolder]") {
 	REQUIRE(isFolder("./dist") == true);
 }
 
+TEST_CASE("isMarkDown() Testing (not folder)", "[isfolder]") {
+	REQUIRE(isFolder("The Adventure of the Speckled Band.txt") == false);
+	REQUIRE(isFolder("The Adventure of the Speckled Band.json") == false);
+}
+
 TEST_CASE("isJson() Testing", "[isjson]") {
 	REQUIRE(isJson("ssg-config.json") == true);
+}
+
+TEST_CASE("isJson() Testing (not .json file)", "[isjson]") {
+	REQUIRE(isJson("The Adventure of the Speckled Band.txt") == false);
+	REQUIRE(isJson("The Adventure of the Speckled Band") == false);
 }
 
 TEST_CASE("getJsonValue() Testing", "[getjsonvalue]") {
